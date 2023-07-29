b943dbe40d3520274bc8ee96ae4a671f7c0c1ff1
Petros Angelatos <petrosagg@gmail.com>
2023-07-30 02:22:30 +0300

    implement handle with channels

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,41 @@
 use std::future::Future;
 use std::mem::ManuallyDrop;
 use std::pin::Pin;
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
 
+use futures_channel::mpsc::{UnboundedReceiver, UnboundedSender};
+use futures_channel::oneshot::Sender;
+use futures_core::Stream;
 use pin_project_lite::pin_project;
 use scoped_trace::Trace;
 
-static COLLECT_TRACE: AtomicBool = AtomicBool::new(false);
-
 pub fn traced<F: Future>(fut: F) -> (TracedTask<F>, TraceHandle) {
-    let slot = Arc::new(Mutex::new(None));
-    let waker = Arc::new(Mutex::new(None));
-    let handle = TraceHandle {
-        slot: Arc::clone(&slot),
-        waker: Arc::clone(&waker),
-    };
-
-    let task = TracedTask { fut, slot, waker };
-
+    let (req_tx, req_rx) = futures_channel::mpsc::unbounded();
+    let handle = TraceHandle { req_tx };
+    let task = TracedTask { fut, req_rx };
     (task, handle)
 }
 
-pub fn enable_tracing() {
-    COLLECT_TRACE.store(true, Ordering::Release);
-}
-
-pub fn disable_tracing() {
-    COLLECT_TRACE.store(true, Ordering::Release);
-}
-
 pub struct TraceHandle {
-    slot: Arc<Mutex<Option<Trace>>>,
-    waker: Arc<Mutex<Option<Waker>>>,
+    req_tx: UnboundedSender<TraceRequest>,
 }
 
 impl TraceHandle {
-    pub fn poke(&self) {
-        let waker = self.waker.lock().unwrap();
-        if let Some(waker) = &*waker {
-            waker.wake_by_ref();
-        }
-    }
-
-    pub fn backtrace(&self) -> Option<Trace> {
-        self.slot.lock().unwrap().take()
+    pub async fn backtrace(&self) -> Option<Trace> {
+        let (tx, rx) = futures_channel::oneshot::channel();
+        self.req_tx.unbounded_send(TraceRequest(tx)).ok()?;
+        rx.await.ok()
     }
 }
+
+struct TraceRequest(Sender<Trace>);
 
 pin_project! {
     pub struct TracedTask<F> {
         #[pin]
         fut: F,
-        slot: Arc<Mutex<Option<Trace>>>,
-        waker: Arc<Mutex<Option<Waker>>>,
+        #[pin]
+        req_rx: UnboundedReceiver<TraceRequest>,
     }
 }
 
@@ -64,11 +45,8 @@ impl<F: Future> Future for TracedTask<F> {
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let this = self.project();
 
-        if COLLECT_TRACE.load(Ordering::Acquire) {
-            let trace_waker = TraceWaker {
-                inner: cx.waker(),
-                slot: &this.slot,
-            };
+        if let Poll::Ready(Some(req)) = this.req_rx.poll_next(cx) {
+            let trace_waker = TracedWaker(cx.waker());
             let raw_waker =
                 RawWaker::new(&trace_waker as *const _ as *const (), &TRACE_WAKER_VTABLE);
             // SAFETY: RawWaker is well formed
@@ -76,63 +54,52 @@ impl<F: Future> Future for TracedTask<F> {
             let mut traced_cx = Context::from_waker(&waker);
 
             let (result, trace) = Trace::root(|| this.fut.poll(&mut traced_cx));
-            let mut slot = this.slot.lock().unwrap();
-            *slot = Some(trace);
+            let _ = req.0.send(trace);
             result
         } else {
-            // In the case where traces are not being collected we will just stash the current
-            // waker in order to cause a wake up in the future when traces are needed.
-            {
-                let mut waker = this.waker.lock().unwrap();
-                *waker = Some(cx.waker().clone());
-            }
-
             this.fut.poll(cx)
         }
     }
 }
 
-struct TraceWaker<'a> {
-    inner: &'a Waker,
-    slot: &'a Mutex<Option<Trace>>,
-}
+struct TracedWaker<'a>(&'a Waker);
 
 const TRACE_WAKER_VTABLE: RawWakerVTable =
     RawWakerVTable::new(clone_raw, wake_raw, wake_by_ref_raw, drop_raw);
 
-/// Clones a TraceWaker . Crucially, cloning converts it into the original wrapped
+/// Clones a TracedWaker. Crucially, cloning converts it into the original wrapped Waker.
 ///
 /// SAFETY:
-/// The data pointer must point to a valid TraceWaker
+/// The data pointer must point to a valid TracedWaker
 unsafe fn clone_raw(data: *const ()) -> RawWaker {
     Trace::leaf();
 
-    let this: &TraceWaker = &*(data as *const TraceWaker);
-    let waker: Waker = this.inner.clone();
+    let this: &TracedWaker = &*(data as *const TracedWaker);
+    let waker: Waker = this.0.clone();
     // SAFETY: Waker is a repr(transparent) over RawWaker
     std::mem::transmute::<Waker, RawWaker>(waker)
 }
 
 /// SAFETY:
-/// The data pointer must point to a valid TraceWaker
+/// The data pointer must point to a valid TracedWaker
 unsafe fn wake_by_ref_raw(data: *const ()) {
     Trace::leaf();
-    let this: &TraceWaker = &*(data as *const TraceWaker);
-    this.inner.wake_by_ref();
+    let this: &TracedWaker = &*(data as *const TracedWaker);
+    this.0.wake_by_ref();
 }
 
-/// No one can end up holding an owned Waker that is backed by a TraceWaker since cloning the
-/// original &Waker (backed by a TraceWaker) produces an owned Waker that is backed by the cloned
+/// No one can end up holding an owned Waker that is backed by a TracedWaker since cloning the
+/// original &Waker (backed by a TracedWaker) produces an owned Waker that is backed by the cloned
 /// *inner* Waker. Therefore this method will never get called.
 unsafe fn wake_raw(_data: *const ()) {
-    unreachable!("an owned Waker backed by TraceWaker is never constructed");
+    unreachable!("an owned Waker backed by TracedWaker is never constructed");
 }
 
-/// No one can end up holding an owned Waker that is backed by a TraceWaker since cloning the
-/// original &Waker (backed by a TraceWaker) produces an owned Waker that is backed by the cloned
+/// No one can end up holding an owned Waker that is backed by a TracedWaker since cloning the
+/// original &Waker (backed by a TracedWaker) produces an owned Waker that is backed by the cloned
 /// *inner* Waker. Therefore this method will never get called.
 unsafe fn drop_raw(_data: *const ()) {
-    unreachable!("an owned Waker backed by TraceWaker is never constructed");
+    unreachable!("an owned Waker backed by TracedWaker is never constructed");
 }
 
 #[cfg(test)]
@@ -145,19 +112,43 @@ mod tests {
         // First spawn a task that will get stuck
         let (task, handle) = traced(std::future::pending::<()>());
         tokio::task::spawn(task);
+        println!("{}", handle.backtrace().await.unwrap());
+    }
 
-        // Verify that we don't have any backtraces yet
-        tokio::time::sleep(Duration::from_millis(10)).await;
-        assert!(handle.backtrace().is_none());
+    #[inline(never)]
+    async fn foo() {
+        bar().await;
+    }
 
-        // Enable tracing and verify that we still don't have a backtrace
-        enable_tracing();
-        tokio::time::sleep(Duration::from_millis(10)).await;
-        assert!(handle.backtrace().is_none());
+    #[inline(never)]
+    async fn bar() {
+        baz().await;
+    }
 
-        // Poke the task which should now collect a backtrace
-        handle.poke();
-        tokio::time::sleep(Duration::from_millis(10)).await;
-        println!("{}", handle.backtrace().unwrap());
+    #[inline(never)]
+    async fn baz() {
+        tokio::time::sleep(Duration::from_secs(u64::MAX)).await;
+    }
+
+    #[tokio::test]
+    async fn it_works2() {
+        let (task1, task1_trace) = traced(foo());
+        let (task2, task2_trace) = traced(foo());
+        tokio::select! {
+            // run the following branches in order of their appearance
+            biased;
+
+            // spawn task #1
+            _ = tokio::spawn(task1) => { unreachable!() }
+
+            // spawn task #2
+            _ = tokio::spawn(task2) => { unreachable!() }
+
+            // print the running tasks
+            _ = tokio::spawn(async {}) => {
+                println!("{}", task1_trace.backtrace().await.unwrap());
+                println!("{}", task2_trace.backtrace().await.unwrap());
+            }
+        }
     }
 }
