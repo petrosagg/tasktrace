8a3f0e4afcd23d1a5e575b2c343656fedbeac069
Petros Angelatos <petrosagg@gmail.com>
2023-07-30 11:12:25 +0300

    add docs and example
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

---
+++ examples/taskdump.rs
@@ -0,0 +1,35 @@
+#[tokio::main]
+async fn main() {
+    let (foo_fut, trace_handle) = backtrace_waker::traced(foo());
+    tokio::spawn(foo_fut);
+
+    println!("{}", trace_handle.backtrace().await.unwrap());
+}
+
+async fn pending() {
+    let mut waker = None;
+    std::future::poll_fn(|cx| {
+        waker = Some(cx.waker().clone());
+        std::task::Poll::Pending
+    }).await
+}
+
+async fn foo() {
+    bar().await;
+}
+
+async fn bar() {
+    tokio::join!(fiz(), buz());
+}
+
+async fn fiz() {
+    pending().await;
+}
+
+async fn buz() {
+    baz().await;
+}
+
+async fn baz() {
+    pending().await;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,90 @@
+//! Logical 'stack' traces of async functions.
+//!
+//! This crate gives you the ability to capture a stacktrace of a running async task.
+//!
+//! ## Usage
+//!
+//! To use, wrap the future you're interested in (usually the top level one) with
+//! `backtrace_waker::trace` and use the returned handle to request a backtrace at any time.
+//!
+//! ```rust
+//! #[tokio::main]
+//! async fn main() {
+//!     let (foo_fut, trace_handle) = backtrace_waker::traced(foo());
+//!     tokio::spawn(foo_fut);
+//!
+//!     println!("{}", trace_handle.backtrace().await.unwrap());
+//! }
+//!
+//! async fn pending() {
+//!     let mut waker = None;
+//!     std::future::poll_fn(|cx| {
+//!         // Pretend to stash the waker for some future time like a real leaf Future would do.
+//!         // This is what records the stacktrace.
+//!         waker = Some(cx.waker().clone());
+//!         std::task::Poll::Pending
+//!     }).await
+//! }
+//!
+//! async fn foo() {
+//!     bar().await;
+//! }
+//!
+//! async fn bar() {
+//!     tokio::join!(fiz(), buz());
+//! }
+//!
+//! async fn fiz() {
+//!     pending().await;
+//! }
+//!
+//! async fn buz() {
+//!     baz().await;
+//! }
+//!
+//! async fn baz() {
+//!     pending().await;
+//! }
+//! ```
+//!
+//! This example program will print out something along the lines of:
+//!
+//! ```text
+//! ╼ <backtrace_waker::TracedTask<F> as core::future::future::Future>::poll::{{closure}} at /home/petrosagg/projects/backtrace-waker/src/lib.rs:116:50
+//!  └╼ taskdump::foo::{{closure}} at /home/petrosagg/projects/backtrace-waker/examples/taskdump.rs:18:11
+//!     └╼ taskdump::bar::{{closure}} at /home/petrosagg/projects/backtrace-waker/examples/taskdump.rs:22:5
+//!        └╼ <tokio::future::poll_fn::PollFn<F> as core::future::future::Future>::poll at /home/petrosagg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tokio-1.29.1/src/future/poll_fn.rs:58:9
+//!           ├╼ taskdump::bar::{{closure}}::{{closure}} at /home/petrosagg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tokio-1.29.1/src/macros/join.rs:126:24
+//!           │  └╼ <tokio::future::maybe_done::MaybeDone<Fut> as core::future::future::Future>::poll at /home/petrosagg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tokio-1.29.1/src/future/maybe_done.rs:68:48
+//!           │     └╼ taskdump::fiz::{{closure}} at /home/petrosagg/projects/backtrace-waker/examples/taskdump.rs:26:15
+//!           │        └╼ taskdump::pending::{{closure}} at /home/petrosagg/projects/backtrace-waker/examples/taskdump.rs:14:8
+//!           │           └╼ <core::future::poll_fn::PollFn<F> as core::future::future::Future>::poll at /rustc/8ede3aae28fe6e4d52b38157d7bfe0d3bceef225/library/core/src/future/poll_fn.rs:64:9
+//!           │              └╼ taskdump::pending::{{closure}}::{{closure}} at /home/petrosagg/projects/backtrace-waker/examples/taskdump.rs:12:22
+//!           │                 └╼ <core::task::wake::Waker as core::clone::Clone>::clone at /rustc/8ede3aae28fe6e4d52b38157d7bfe0d3bceef225/library/core/src/task/wake.rs:342:29
+//!           │                    └╼ backtrace_waker::clone_raw at /home/petrosagg/projects/backtrace-waker/src/lib.rs:135:5
+//!           └╼ taskdump::bar::{{closure}}::{{closure}} at /home/petrosagg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tokio-1.29.1/src/macros/join.rs:126:24
+//!              └╼ <tokio::future::maybe_done::MaybeDone<Fut> as core::future::future::Future>::poll at /home/petrosagg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tokio-1.29.1/src/future/maybe_done.rs:68:48
+//!                 └╼ taskdump::buz::{{closure}} at /home/petrosagg/projects/backtrace-waker/examples/taskdump.rs:30:11
+//!                    └╼ taskdump::baz::{{closure}} at /home/petrosagg/projects/backtrace-waker/examples/taskdump.rs:34:15
+//!                       └╼ taskdump::pending::{{closure}} at /home/petrosagg/projects/backtrace-waker/examples/taskdump.rs:14:8
+//!                          └╼ <core::future::poll_fn::PollFn<F> as core::future::future::Future>::poll at /rustc/8ede3aae28fe6e4d52b38157d7bfe0d3bceef225/library/core/src/future/poll_fn.rs:64:9
+//!                             └╼ taskdump::pending::{{closure}}::{{closure}} at /home/petrosagg/projects/backtrace-waker/examples/taskdump.rs:12:22
+//!                                └╼ <core::task::wake::Waker as core::clone::Clone>::clone at /rustc/8ede3aae28fe6e4d52b38157d7bfe0d3bceef225/library/core/src/task/wake.rs:342:29
+//!                                   └╼ backtrace_waker::clone_raw at /home/petrosagg/projects/backtrace-waker/src/lib.rs:135:5
+//! ```
+//!
+//! ## How it works
+//!
+//! This crate exploits the fact that all leaf futures must clone the provided context Waker in
+//! order to call it at some later time when progress can be made.
+//!
+//! When a traced future is requested to capture a callstack it wraps the normal waker into one
+//! whose `Waker::clone` method is set up to capture a backtrace. Since only leaf futures ever
+//! interact and clone the waker the stacktraces will necessarily contain the desired callstack.
+//!
+//! If multiple futures are being waited on (e.g through `select!`) then multiple stacktraces will
+//! be captured for each polled future and their combined stacktrace will be displayed as a tree.
+
 use std::future::Future;
 use std::mem::ManuallyDrop;
 use std::pin::Pin;
@@ -73,7 +160,6 @@ const TRACE_WAKER_VTABLE: RawWakerVTable =
 /// The data pointer must point to a valid TracedWaker
 unsafe fn clone_raw(data: *const ()) -> RawWaker {
     Trace::leaf();
-
     let this: &TracedWaker = &*(data as *const TracedWaker);
     let waker: Waker = this.0.clone();
     // SAFETY: Waker is a repr(transparent) over RawWaker
@@ -105,50 +191,40 @@ unsafe fn drop_raw(_data: *const ()) {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::time::Duration;
 
     #[tokio::test]
     async fn it_works() {
-        // First spawn a task that will get stuck
-        let (task, handle) = traced(std::future::pending::<()>());
-        tokio::task::spawn(task);
-        println!("{}", handle.backtrace().await.unwrap());
+        let (foo_fut, trace_handle) = traced(foo());
+        tokio::spawn(foo_fut);
+
+        println!("{}", trace_handle.backtrace().await.unwrap());
     }
 
-    #[inline(never)]
+    async fn pending() {
+        let mut waker = None;
+        std::future::poll_fn(|cx| {
+            waker = Some(cx.waker().clone());
+            std::task::Poll::Pending
+        }).await
+    }
+
     async fn foo() {
         bar().await;
     }
 
-    #[inline(never)]
     async fn bar() {
+        tokio::join!(fiz(), buz());
+    }
+
+    async fn fiz() {
+        pending().await;
+    }
+
+    async fn buz() {
         baz().await;
     }
 
-    #[inline(never)]
     async fn baz() {
-        tokio::time::sleep(Duration::from_secs(u64::MAX)).await;
-    }
-
-    #[tokio::test]
-    async fn it_works2() {
-        let (task1, task1_trace) = traced(foo());
-        let (task2, task2_trace) = traced(foo());
-        tokio::select! {
-            // run the following branches in order of their appearance
-            biased;
-
-            // spawn task #1
-            _ = tokio::spawn(task1) => { unreachable!() }
-
-            // spawn task #2
-            _ = tokio::spawn(task2) => { unreachable!() }
-
-            // print the running tasks
-            _ = tokio::spawn(async {}) => {
-                println!("{}", task1_trace.backtrace().await.unwrap());
-                println!("{}", task2_trace.backtrace().await.unwrap());
-            }
-        }
+        pending().await;
     }
 }
